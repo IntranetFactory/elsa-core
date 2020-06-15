ecbb5191967006873be4f33ae57ec52771575005
Milan Miscevic <mmiscevic@smartnet.rs>
2020-06-15 15:47:13 +0200

    Added comments which explain AdvisoryLockManager methods.

--- src/core/Elsa.Core/Services/AdvisoryLockManager.cs
+++ src/core/Elsa.Core/Services/AdvisoryLockManager.cs
@@ -21,21 +21,27 @@ namespace Elsa.Services
             this.logger = logger;
         }
 
+        // Lock is used to create/acquire an advisory lock for the specified workflow_instance_xxx in the database by calling pg_try_advisory_lock command.
+        // If lock is succesfully acquired - it returns true, otherwise - it returns false.
         public async Task<bool> Lock(string name, CancellationToken cancellationToken = default)
         {
             bool lockAcquired = false;
 
             try
             {
+                // open database connection
                 var connection = new NpgsqlConnection(connectionString);
                 await connection.OpenAsync();
+                // get integer value from hashed string "name"
                 var hashedName = await HashName(name, connection, cancellationToken);
-
+                // creates a command to be executed
                 NpgsqlCommand command = new NpgsqlCommand("pg_try_advisory_lock", connection);
                 command.CommandType = CommandType.StoredProcedure;
                 command.Parameters.AddWithValue(hashedName);
+                // specifies the return value of pg_try_advisory_lock command and it's type
                 var advisoryLockReturnValue = command.Parameters.Add("ReturnValue", NpgsqlDbType.Boolean);
                 advisoryLockReturnValue.Direction = ParameterDirection.Output;
+                // execute command
                 await command.ExecuteNonQueryAsync(cancellationToken);
                 lockAcquired = Convert.ToBoolean(advisoryLockReturnValue.Value);
 
@@ -52,13 +58,16 @@ namespace Elsa.Services
         {
             try
             {
+                // open database connection
                 var connection = new NpgsqlConnection(connectionString);
                 await connection.OpenAsync();
+                // get integer value from hashed string "name"
                 var hashedName = await HashName(name, connection, cancellationToken);
-
+                // creates a command to be executed
                 NpgsqlCommand command = new NpgsqlCommand("pg_advisory_unlock", connection);
                 command.CommandType = CommandType.StoredProcedure;
                 command.Parameters.AddWithValue(hashedName);
+                // execute command
                 await command.ExecuteNonQueryAsync(cancellationToken);
             }
             catch (Exception ex)
@@ -67,11 +76,17 @@ namespace Elsa.Services
             }
         }
 
+
+        // HashName is used to return an integer value by hashing a string "name". 
+        // This is used because advisory locking functions require integers instead of strings.
         private async Task<object> HashName(string name, NpgsqlConnection connection, CancellationToken cancellationToken = default)
         {
+            // creates a command to be executed
             NpgsqlCommand command = new NpgsqlCommand("hashtext", connection);
             command.CommandType = CommandType.StoredProcedure;
+            // provides parameter value to the command
             command.Parameters.AddWithValue(name);
+            // specifies the return value of hashtext command and it's type
             var hashedName = command.Parameters.Add("ReturnValue", NpgsqlDbType.Integer);
             hashedName.Direction = ParameterDirection.Output;
             await command.ExecuteNonQueryAsync(cancellationToken);
